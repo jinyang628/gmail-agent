697c3023c5ae682a1148ffa1c20017253eebcd86
jinyang628 <chenjinyang4192@gmail.com>
2025-06-21 11:38:11 +0100

    Use most updated param shape for OAuth2 constructor

--- api/test.ts
+++ api/test.ts
@@ -3,11 +3,10 @@ import { google } from 'googleapis';
 
 const gmail = google.gmail('v1');
 
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  'http://localhost:3000/auth/callback',
-);
+const oauth2Client = new google.auth.OAuth2({
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
 
 oauth2Client.setCredentials({
   refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
